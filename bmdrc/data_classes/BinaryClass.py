5a0f12233eabd9600ba4ccb0ddc6901c691a70dc
david.degnan@pnnl.gov <degn400@we45748.pnl.gov>
2023-06-08 15:55:46 -0700

    Initial build of BinaryClass pt. 3

--- bmdrc/data_classes/BinaryClass.py
+++ bmdrc/data_classes/BinaryClass.py
@@ -1,4 +1,5 @@
 import operator
+import pandas as pd
 
 __author__ = "David Degnan"
 
@@ -6,17 +7,17 @@ class BinaryClass(object):
     '''
     Generates a binary class object where input values are either a 0 or a 1
 
-    df: (pandas DataFrame) A dataframe containing columns title chemical.ID, 
-    plate.ID, well.ID, concentration, endpoint (long format only), value (long format only).
+    df: (pandas DataFrame) A dataframe containing columns title chemical, 
+    plate, well, concentration, endpoint (long format only), value (long format only).
     If the data is in wide format, all additional columns are assumed to be endpoints.
 
-    chemical.ID: (string) name of the column containing the chemical IDs, which
+    chemical: (string) name of the column containing the chemical IDs, which
     should be strings
 
-    plate.ID: (string) name of the column indicating the plate IDs, which should be
+    plate: (string) name of the column indicating the plate IDs, which should be
     strings
 
-    well.ID: (string) name of the column with the well IDs, which should be strings
+    well: (string) name of the column with the well IDs, which should be strings
 
     concentration: (string) name of the column containing the concentrations, which
     should be numerics
@@ -28,83 +29,104 @@ class BinaryClass(object):
     be 0 for absent, and 1 for present. Not used if the data is in wide format.
 
     format: (string) indicate whether the data is in 'long' or 'wide' format. Wide
-    format requires only the chemical.ID, plate.ID, well.ID, and concentration columns.
+    format requires only the chemical, plate, well, and concentration columns.
     The rest of the columns are assumed to be endpoints. Wide formats are then converted
     to the long format. 
     '''
 
     # Define the input checking functions 
-    def __init__(self, chemicalID, concentration, plateID, well, endpoint, value):
-        self.chemicalID = chemicalID
-        self.concentration = concentration
-        self.plateID = plateID
+    def __init__(self, df, chemical, plate, well, concentration, endpoint, value): #format):
+        self.df = df
+        self.chemical = chemical
+        self.plate = plate
         self.well = well
+        self.concentration = concentration
         self.endpoint = endpoint
         self.value = value
+        #self.format = format
 
     # Set property returning functions 
-    chemicalID = property(operator.attrgetter('_chemicalID'))
-    concentration = property(operator.attrgetter('_concentration'))
-    plateID = property(operator.attrgetter('_plateID'))
+    df = property(operator.attrgetter('_df'))
+    chemical = property(operator.attrgetter('_chemical'))
+    plate = property(operator.attrgetter('_plate'))
     well = property(operator.attrgetter('_well'))
+    concentration = property(operator.attrgetter('_concentration'))
     endpoint = property(operator.attrgetter('_endpoint'))
     value = property(operator.attrgetter('_value'))
+    #format = property(operator.attrgetter('_format'))
 
     # Now, ensure each input is correct 
 
-    @chemicalID.setter
-    def chemicalID(self, chemicalIDs):
-        if not chemicalIDs: 
-            raise Exception("chemicalID cannot be empty. Please provide a list \
-                            of strings representing each chemical's ID")
-        if not isinstance(chemicalIDs, str):
-            raise Exception("chemicalID must be a list of strings.")
-        self._chemicalID = chemicalIDs
-        
-    @concentration.setter
-    def concentration(self, concentrations):
-        if not concentrations: 
-            raise Exception("concentration cannot be empty. Please provide a list \
-                            of numerics representing each chemical's concentration")
-        if not isinstance(concentrations, float):
-            raise Exception("concentration must be a list of numerics.")
-        self._concentration = concentrations
-        
-    @plateID.setter
-    def plateID(self, plateIDs):
-        if not plateIDs: 
-            raise Exception("plateID cannot be empty. Please provide a list \
-                            of strings representing each plateID")
-        if not isinstance(plateIDs, str):
-            raise Exception("plateID must be a list of strings.")
-        self._plateID = plateIDs
+    @df.setter
+    def df(self, theDF):
+        if theDF.empty:
+            raise Exception("df cannot be empty. Please provide a pandas DataFrame.")
+        if not isinstance(theDF, pd.DataFrame):
+            raise Exception("df must be a pandas DataFrame")
+        self._df = theDF
+
+    @chemical.setter
+    def chemical(self, chemicalname):
+        if not chemicalname: 
+           raise Exception("chemical cannot be empty. Please enter the column name for \
+                            the chemicals.")
+        if not isinstance(chemicalname, str):
+            raise Exception("chemical must be a name of a column in df.")
+        if not chemicalname in self._df.columns:
+            raise Exception(chemicalname + "is not in the column names of df")
+        self._chemical = chemicalname
+
+    @plate.setter
+    def plate(self, platename):
+        if not platename: 
+           raise Exception("plate cannot be empty. Please enter the column name for \
+                            the plate ids.")
+        if not isinstance(platename, str):
+            raise Exception("plate must be a name of a column in df.")
+        if not platename in self._df.columns:
+            raise Exception(platename + " is not in the column names of df")
+        self._plate = platename
         
     @well.setter
-    def well(self, wells):
-        if not wells: 
-            raise Exception("well cannot be empty. Please provide a list \
-                            of strings representing each well")
-        if not isinstance(wells, str):
-            raise Exception("well must be a list of strings.")
-        self._well = wells
+    def well(self, wellname):
+        if not wellname: 
+           raise Exception("well cannot be empty. Please enter the column name for \
+                            the well ids.")
+        if not isinstance(wellname, str):
+            raise Exception("well must be a name of a column in df.")
+        if not wellname in self._df.columns:
+            raise Exception(wellname + " is not in the column names of df")
+        self._well = wellname
+        
+    @concentration.setter
+    def concentration(self, concentrationname):
+        if not concentrationname: 
+           raise Exception("concentration cannot be empty. Please enter the column name for \
+                            the concentration.")
+        if not isinstance(concentrationname, str):
+            raise Exception("concentration must be a name of a column in df.")
+        if not concentrationname in self._df.columns:
+            raise Exception(concentrationname + " is not in the column names of df")
+        self._df[concentrationname] = pd.to_numeric(self._df[concentrationname])
+        self._concentration = concentrationname
         
     @endpoint.setter
     def endpoint(self, endpoints):
-        if not endpoints: 
-            raise Exception("endpoint cannot be empty. Please provide a list \
-                            of strings representing each endpoint.")
-        if not isinstance(endpoints, str):
-            raise Exception("endpoint must be a list of strings.")
+    #    if not endpoints: 
+    #        raise Exception("endpoint cannot be empty. Please provide a list \
+    #                        of strings representing each endpoint.")
+    #    if not isinstance(endpoints, str):
+    #        raise Exception("endpoint must be a list of strings.")
         self._endpoint = endpoints
         
         
     @value.setter
     def value(self, values):
-        if not values: 
-            raise Exception("value cannot be empty. Please provide a list \
-                            of zeroes and ones representing whether a condition \
-                            is absent or present, respectively.")
-        if all(val == 0 or val == 1 for val in values):
-            raise Exception("value must be a list of zeroes or ones indicating \
-                            whether a condition is absent or present, respectively.")
+    #    if not values: 
+     #       raise Exception("value cannot be empty. Please provide a list \
+     #                       of zeroes and ones representing whether a condition \
+     #                       is absent or present, respectively.")
+     #   if all(val == 0 or val == 1 for val in values):
+     #       raise Exception("value must be a list of zeroes or ones indicating \
+     #                       whether a condition is absent or present, respectively.")
         self._value = values
