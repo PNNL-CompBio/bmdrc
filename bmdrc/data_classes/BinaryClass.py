dcb2efe3404e72a0992c1fbef37b25df79a3e9f8
david.degnan@pnnl.gov <degn400@we45748.pnl.gov>
2023-06-08 11:58:57 -0700

    Initial build of BinaryClass pt. 1

--- bmdrc/data_classes/BinaryClass.py
+++ bmdrc/data_classes/BinaryClass.py
@@ -4,18 +4,33 @@ __author__ = "David Degnan"
 
 class BinaryClass(object):
     '''
-    Generates a binary class object where input values are either a 0 or a 1, 
-    which requires a designation for the following columns:
+    Generates a binary class object where input values are either a 0 or a 1
 
-    1. chemicalID
-    2. concentration
-    3. plateID
-    4. well
-    5. endpoint
-    6. value --> here, should be a 0 or a 1
+    df: (pandas DataFrame) A dataframe containing columns title chemical.ID, 
+    plate.ID, well.ID, concentration, endpoint (long format only), value (long format only).
+    If the data is in wide format, all additional columns are assumed to be endpoints.
 
-    If the data is in long format, the "endpoint" and "value" designations will be
-    created before the class call. 
+    chemical.ID: (string) name of the column containing the chemical IDs, which
+    should be strings
+
+    plate.ID: (string) name of the column indicating the plate IDs, which should be
+    strings
+
+    well.ID: (string) name of the column with the well IDs, which should be strings
+
+    concentration: (string) name of the column containing the concentrations, which
+    should be numerics
+
+    endpoint: (string) name of the column containing endpoints, which should be
+    a string. Not used if the data is in wide format. 
+
+    value: (string) name of the column containing the binary values, which should 
+    be 0 for absent, and 1 for present. Not used if the data is in wide format.
+
+    format: (string) indicate whether the data is in 'long' or 'wide' format. Wide
+    format requires only the chemical.ID, plate.ID, well.ID, and concentration columns.
+    The rest of the columns are assumed to be endpoints. Wide formats are then converted
+    to the long format. 
     '''
 
     # Define the input checking functions 
