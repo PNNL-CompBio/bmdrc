51bfe35acd1b67234c5fc32129c3101ef6ddda8c
david.degnan@pnnl.gov <degn400@we45748.pnl.gov>
2023-06-08 11:15:54 -0700

    Designing data upload

--- bmdrc/data_classes/BinaryClass.py
+++ bmdrc/data_classes/BinaryClass.py
@@ -1,11 +1,8 @@
 import operator
-import pandas as pd
-import numpy as np
-import sys 
 
 __author__ = "David Degnan"
 
-class Binary(object):
+class BinaryClass(object):
     '''
     Generates a binary class object where input values are either a 0 or a 1, 
     which requires a designation for the following columns:
@@ -47,3 +44,52 @@ class Binary(object):
                             of strings representing each chemical's ID")
         if not isinstance(chemicalIDs, str):
             raise Exception("chemicalID must be a list of strings.")
+        self._chemicalID = chemicalIDs
+        
+    @concentration.setter
+    def concentration(self, concentrations):
+        if not concentrations: 
+            raise Exception("concentration cannot be empty. Please provide a list \
+                            of numerics representing each chemical's concentration")
+        if not isinstance(concentrations, float):
+            raise Exception("concentration must be a list of numerics.")
+        self._concentration = concentrations
+        
+    @plateID.setter
+    def plateID(self, plateIDs):
+        if not plateIDs: 
+            raise Exception("plateID cannot be empty. Please provide a list \
+                            of strings representing each plateID")
+        if not isinstance(plateIDs, str):
+            raise Exception("plateID must be a list of strings.")
+        self._plateID = plateIDs
+        
+    @well.setter
+    def well(self, wells):
+        if not wells: 
+            raise Exception("well cannot be empty. Please provide a list \
+                            of strings representing each well")
+        if not isinstance(wells, str):
+            raise Exception("well must be a list of strings.")
+        self._well = wells
+        
+    @endpoint.setter
+    def endpoint(self, endpoints):
+        if not endpoints: 
+            raise Exception("endpoint cannot be empty. Please provide a list \
+                            of strings representing each endpoint.")
+        if not isinstance(endpoints, str):
+            raise Exception("endpoint must be a list of strings.")
+        self._endpoint = endpoints
+        
+        
+    @value.setter
+    def value(self, values):
+        if not values: 
+            raise Exception("value cannot be empty. Please provide a list \
+                            of zeroes and ones representing whether a condition \
+                            is absent or present, respectively.")
+        if all(val == 0 or val == 1 for val in values):
+            raise Exception("value must be a list of zeroes or ones indicating \
+                            whether a condition is absent or present, respectively.")
+        self._value = values
