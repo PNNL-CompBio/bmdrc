b12a3482d3608ed02969e219cf094f675e23a3cf
david.degnan@pnnl.gov <degn400@we45748.lan>
2024-10-08 17:19:34 -0700

    Building out tests

--- tests/binary_class_tests.py
+++ tests/binary_class_tests.py
@@ -1,14 +1,15 @@
 import pandas as pd
+import pytest
 from bmdrc import BinaryClass
 
 ## How to run test functions: 
-# coverage run -m bmdrc tests/unit_tests.py     
+# coverage run -m bmdrc.BinaryClass tests/binary_class_tests.py
 # coverage report
 # coverage html
 
-
 ## Binary Class Tests ## 
 
+# Test to ensure the function runs without error
 def make_BinaryClass():
     BinaryClass(
         df = pd.read_csv("../data/Binary_Simplified_Long.csv").drop("Notes", axis = 1), # Input is a pandas DataFrame
@@ -20,10 +21,21 @@ def make_BinaryClass():
         value = "value", # The name of the column with values
         format = "long" # The format of the input data, either 'long' or 'wide' is accepted
     )
-
-def test_BinaryClass():
     
-    # Make the binary class object
-    BC = make_BinaryClass()
+# Test errors 
+def test_df():
+
+    BinaryClass.BinaryClass(
+        df = pd.DataFrame(),
+        chemical = "chemical.id", # The name of the chemical column 
+        plate = "plate.id", # The name of the plate ID column
+        well = "well", # The name of the column with well names
+        concentration = "concentration", # The name of the concentration column
+        endpoint = "endpoint", # The name of the column with endpoints
+        value = "value", # The name of the column with values
+        format = "long" # The format of the input data, either 'long' or 'wide' is accepted
+    )
+
+    
 
 
