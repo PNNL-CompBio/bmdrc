2d556ae65d7c1af907333108e939e6b1cce3b991
david.degnan@pnnl.gov <degn400>
2024-10-09 10:33:44 -0700

    Building out tests

--- tests/binary_class_tests.py
+++ tests/binary_class_tests.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import pytest
+import bmdrc
 from bmdrc import BinaryClass
 
-## How to run test functions: 
-# coverage run -m bmdrc.BinaryClass tests/binary_class_tests.py
+## How to calculate coverage (from within main package directory): 
+# coverage run --source=bmdrc -m pytest -x tests/*
 # coverage report
 # coverage html
 
@@ -11,22 +12,9 @@ from bmdrc import BinaryClass
 
 # Test to ensure the function runs without error
 def make_BinaryClass():
-    BinaryClass(
-        df = pd.read_csv("../data/Binary_Simplified_Long.csv").drop("Notes", axis = 1), # Input is a pandas DataFrame
-        chemical = "chemical.id", # The name of the chemical column 
-        plate = "plate.id", # The name of the plate ID column
-        well = "well", # The name of the column with well names
-        concentration = "concentration", # The name of the concentration column
-        endpoint = "endpoint", # The name of the column with endpoints
-        value = "value", # The name of the column with values
-        format = "long" # The format of the input data, either 'long' or 'wide' is accepted
-    )
-    
-# Test errors 
-def test_df():
 
-    BinaryClass.BinaryClass(
-        df = pd.DataFrame(),
+    LongTest = BinaryClass.BinaryClass(
+        df = pd.read_csv("data/Binary_Simplified_Long.csv").drop("Notes", axis = 1), # Input is a pandas DataFrame
         chemical = "chemical.id", # The name of the chemical column 
         plate = "plate.id", # The name of the plate ID column
         well = "well", # The name of the column with well names
@@ -36,6 +24,36 @@ def test_df():
         format = "long" # The format of the input data, either 'long' or 'wide' is accepted
     )
 
+    WideTest = BinaryClass.BinaryClass(
+        df = pd.read_csv("data/Binary_Morphology_Wide.csv"),
+        chemical = "chemical.id",
+        plate = "plate.id",
+        well = "well",
+        concentration = "conc",
+        endpoint = "endpoint",
+        value = "value",
+        format = "wide"
+    )
     
+# Test wrong inputs 
+def test_wrong_inputs():
 
+    with pytest.raises(Exception, match = "df cannot be empty. Please provide a pandas DataFrame.") as test_info:
+        BinaryClass.BinaryClass(
+            df = pd.DataFrame(),
+            chemical = "chemical.id", 
+            plate = "plate.id", 
+            well = "well", 
+            concentration = "concentration", 
+            endpoint = "endpoint",
+            value = "value", 
+            format = "long"
+        )
+    assert test_info.type == Exception
+
+def run_tests():
+    make_BinaryClass()
+    test_wrong_inputs()
+
+run_tests()
 
